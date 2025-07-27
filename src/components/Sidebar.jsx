dbe77f242181a310947dee00280bc2fc4e3bbd47
midi64 <Mahdishokri64@gmail.com>
2025-07-27 20:40:28 +0200

    add pages components styles

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+import { createQueryObject } from "../helpers/helper";
+
+import { FaListUl } from "react-icons/fa";
+import styles from "./Sidebar.module.css";
+import { categories } from "../constants/list";
+
+
+function Sidebar({ setQuery, query }) {
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+    console.log(category);
+    if (tagName != "LI") return;
+
+    setQuery((query) => createQueryObject(query, { category }));
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <div>
+        <FaListUl />
+        <p>Categories</p>
+      </div>
+      <ul onClick={categoryHandler}>
+        {categories.map((item) => (
+          <li
+            key={item.id}
+            className={
+              item.type.toLowerCase() === query.category
+                ? styles.selected
+                : null
+            }
+          >
+            {item.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
